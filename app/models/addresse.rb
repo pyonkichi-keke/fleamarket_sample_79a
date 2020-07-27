1f7ebc3cff5ff3982bd7bd452ee6f9e434f4ed91
pyonkichi-keke <pyonkichi.keke.sakura@gmail.com>
2020-07-27 12:54:15 +0900

    DB設計

--- app/models/addresse.rb
+++ app/models/addresse.rb
@@ -1,2 +1,16 @@
 class Addresse < ApplicationRecord
+
+  belongs_to user
+
+  
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/ }
+  validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  validates :prefecture, presence: true 
+  validates :city, presence: true
+  validates :address, presence: true 
+  validates :apartment, absence: true
+  validates :tell, absence: true, format: { with: /\A\d{10,11}\z/ }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-# validates :nickname presence: true
-# validates :email, presence: true, uniqueness: true, confimation: true, format:{with: VALID_EMAIL}
-# validates :encrypted_password, presence: true, confirmation: true, length: { minimum: 7 }
-# validates :encrypted_password, password_confirmation: true
-# validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-# validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-# validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/ }
-# validates :last_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/ } 
-# validates :birthday, presence: true, length: { is: 8 }, format: { with: /\A[0-9]+\z/ }
+         
+      has_many :commnets
+      has_many :products
+      has_many :likes_product, through: :likes, source: :product
+      has_many :comments, dependent: :destroy
+      has_many :likes, dependent: :destroy
+      has_many :products, dependent: :destroy
+      has_one :credit_card, dependent: :destroy
+      has_one :address, dependent: :destroy         
+
+
+      VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+      validates :nickname, presence: true
+      validates :email, presence: true, uniqueness: true, format:{with: VALID_EMAIL}
+      validates :password, presence: true, confirmation: true, length: { minimum: 7 }
+      validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+      validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+      validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/ }
+      validates :last_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/ } 
+      validates :birthday, presence: true, length: { is: 8 }, format: { with: /\A[0-9]+\z/ }
 
 end
