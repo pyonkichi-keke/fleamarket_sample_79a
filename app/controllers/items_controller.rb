d34a98674a8dd82c1239d4c249319217009cfc03
yusuke112332 <yusuke123491@gmail.com>
2020-07-30 10:35:00 +0900

    サーバーサイド画像表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
   def index
+    # @products = Product.all.includes(:images)
+    @products = Product.order('created_at DESC').limit(5)
+    @images = Image.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
 
   root 'items#index'
   
+  resources :items, only: :index
 end
