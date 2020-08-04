d0f8210b261ea4d14350959615412bca3613e54e
pyonkichi-keke <pyonkichi.keke.sakura@gmail.com>
2020-08-04 14:10:07 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++
@@ -1,7 +0,0 @@
-class ItemsController < ApplicationController
-  def index
-    # @products = Product.all.includes(:images)
-    @products = Product.order('created_at DESC').limit(5)
-    @images = Image.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   root 'homes#index'
   
-  resources :items, only: :index
+  resources :products
 end
